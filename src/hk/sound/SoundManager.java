51248b5bf2e4f36d22ee567280b1a58824cdfe0d
Hassan <hassan_99@live.dk>
2022-06-13 08:08:21 +0200

    SoundManager to handle the sound and music in the application

---
+++ src/hk/sound/SoundManager.java
@@ -0,0 +1,47 @@
+package hk.sound;
+
+import javax.sound.sampled.*;
+import javax.swing.*;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SoundManager extends JFrame {
+
+    private Clip clip;
+
+    public static boolean TICKEDOFF;
+
+    public SoundManager(String fileName) {
+        if (!TICKEDOFF) {
+
+            try {
+
+                var url = new URL("./resources" + fileName + ".wav");
+                var audio = AudioSystem.getAudioInputStream(url);
+
+                clip = AudioSystem.getClip();
+                clip.open(audio);
+
+                if (fileName.startsWith("ambulance")) {
+                    FloatControl floatControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                    float db = (float) (Math.log(0.1) / Math.log(10) * 20);
+                    floatControl.setValue(db);
+                }
+                clip.start();
+                clip.loop((fileName.equalsIgnoreCase("plaque.wav") || fileName.equalsIgnoreCase("ambulance.wav") || fileName.startsWith("ambulance")) ? 4 : 0);
+
+            } catch (LineUnavailableException | UnsupportedAudioFileException | IOException e) {
+                System.err.println("ERROR! Something went wrong with the sound file: " + fileName + ": " + e);
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public void stop() {
+        clip.stop();
+        clip.flush();
+    }
+
+}
